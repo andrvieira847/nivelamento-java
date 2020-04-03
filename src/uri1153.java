047b6d2de16708dc750616b9cbbf9b2294baa93c
andrvieira847 <61885815+andrvieira847@users.noreply.github.com>
2020-04-03 10:01:15 -0300

    Update and rename src/uri1153.java to  src/uri1153.java

--- src/uri1153.java
+++ src/uri1153.java
@@ -1,20 +1,22 @@
-import java.util.Scanner;
 
-public class uri1153 {
+ import java.util.Scanner;
+
+public class Main {
 
 	public static void main(String[] args) {
-
+		
+		//Ler um valor N. Calcular e escrever seu respectivo fatorial.
+		//Fatorial de N = N * (N-1) * (N-2) * (N-3) * ... * 1.
+		//Lembrando que, por definição, fatorial de 0 é 1
+		
 		Scanner sc = new Scanner(System.in);
-
+       
 		int n = sc.nextInt();
-		
-		int fat = 1;
-		for (int i=1; i<=n; i++) {
-			fat = fat * i;
+		int fatorial = 1;
+		for (int i = 0; i < n; i++) {
+			fatorial = fatorial * (n-i);
 		}
-		
 		System.out.println(fat);
-		
 		sc.close();
 	}
 }
